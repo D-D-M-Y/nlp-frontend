27112572f195951b12a01f7e1f4ac3efff865008
Allana Yzabelle Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2024-05-06 03:33:06 +0800

    added overview page in right side content, deleted overview under headers array

--- src/app/(pages)/wiki-generator/page.tsx
+++ src/app/(pages)/wiki-generator/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import { Chatbot } from '@/app/components/chatbot';
 import Builder from '@/app/components/builder';
+import Overview from '@/app/components/overview';
 
 // Define the MessageState interface
 interface MessageState {
@@ -30,30 +31,27 @@ const Page = () => {
   // Define the topic variable
   const topic = 'Topic 1';
 
-  // Define the headers array
+  // Define the headers array [to edit]
   const headers = [
     {
       header: 'header1',
       subheader: 'subheader1',
       page: 'page1',
-      overview: 'Overview of page 1',
     },
     {
       header: 'header2',
       subheader: 'subheader2',
       page: 'page2',
-      overview: 'Overview of page 2',
     },
     {
       header: 'header3',
       subheader: 'subheader3',
       page: 'page3',
-      overview: 'Overview of page 3',
     },
   ];
 
   return (
-    <div className="overflow-hidden bg-white p-5 w-full h-screen relative grid grid-cols-2 gap-2 w-full">
+    <div className="overflow-hidden bg-white p-5 w-full h-screen relative grid grid-cols-2 w-full">
       {/* Left Content (1/2 width) */}
       <div className="h-full flex flex-row grid-row-2 w-full gap-4">
         {/* First Column (1/2 width) */}
@@ -66,13 +64,14 @@ const Page = () => {
           <Chatbot messages={messageState.messages} onSendMessage={handleSendMessage} />
         </div>
         {/* Second Column (1/2 width) */}
-        <div className="w-1/2 flex grow flex-col rounded-lg overflow-y-auto font-inter text-textC">
+        <div className="w-1/2 flex grow flex-col overflow-y-auto font-inter text-textC">
           <Builder topic={topic} headers={headers} />
         </div>
       </div>
       {/* Right Side Content (1/2 width)*/}
-      <div className="w-full bg-pastel flex grow flex-col p-4 rounded-lg overflow-y-auto mb-2 font-inter font-bold text-textC text-3xl" 
+      <div className="w-full bg-pastel flex grow flex-col p-4 border-4 border-border border-l-0 rounded-r-xl overflow-y-auto mb-2 font-inter font-bold text-textC text-3xl" 
       >
+        <Overview topic={topic} details={headers}/>
       </div>
     </div>
   );
