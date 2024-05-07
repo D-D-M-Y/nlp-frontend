de34aa5c30deb677419f2f9e687470971e408564
Allana Yzabelle Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2024-05-07 20:10:11 +0800

    separated placeholder components

--- src/app/components/builder.tsx
+++ src/app/components/builder.tsx
@@ -1,38 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 interface BuilderProps {
   /** The topic to be displayed */
   topic: string;
   /** An array of header objects, each containing a header, subheader, and page */
   headers: { header: string; subheader: string; page: string }[];
+  onBuilderDataLoaded: () => void;
 }
 
 /** The Builder component displays a topic and an array of headers */
-const Builder: React.FC<BuilderProps> = ({ topic, headers }) => {
+const Builder: React.FC<BuilderProps> = ({ topic, headers, onBuilderDataLoaded }) => {
   // Set up state to track whether the actual values have been loaded
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  // Display placeholders while waiting for the actual values to be loaded
-  if (!isLoaded) {
-    return (
-      <div className="w-full border-4 border-border border-r-0 rounded-l-xl flex grow flex-col p-4 overflow-y-auto mb-2 font-inter text-textC text-3xl">
-        <div className="pl-2 h-10 mb-4 rounded-xl bg-gray-300 w-1/2"></div>
-        {headers.map((headerObj, index) => (
-          <div key={index}>
-            <div className="ml-8 h-4 mb-2 rounded-xl bg-gray-300  w-1/2"></div>
-            <div className="ml-20 h-4 mb-2 rounded-xl bg-gray-300  w-1/2"></div>
-            <div className="ml-32 h-4 mb-6 rounded-md bg-gray-300  w-1/3"></div>
-          </div>
-        ))}
-      </div>
-    );
-  }
+  onBuilderDataLoaded();
+  
 
   // Display the actual values once they have been loaded [to be edited]
   return (
-    <div className="w-full border-4 border-border border-r-0 rounded-l-xl flex grow flex-col p-4 overflow-y-auto mb-2 font-inter text-textC text-3xl">
+    <div className="flex flex-col grow border-4 border-border rounded-lg pl-5 mt-5 overflow-y-auto mb-2 p-2 text-textC">
       {/* Display the topic */}
-      <h1 className=" font-bold text-3xl italic">{topic}</h1>
+      <h1 className="font-bold text-2xl italic">{topic}</h1>
       {/* Map over the headers array and display each header object */}
       {headers.map((headerObj, index) => (
         <div key={index} className="pl-2">
--- src/app/components/overview.tsx
+++ src/app/components/overview.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react';
 
 interface OverviewProps {
   /** The title to be displayed */
@@ -9,43 +9,19 @@ interface OverviewProps {
 
 /** The Overview component displays an overview of the title and details */
 const Overview: React.FC<OverviewProps> = ({ topic, details }) => {
-  // Set up state to track whether the actual values have been loaded
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  // Display placeholders while waiting for the actual values to be loaded
-  if (!isLoaded) {
-    return (
-      <div className="w-full flex grow flex-col overflow-y-auto mb-2 font-inter text-textC text-3xl">
-        <div className="flex flex-row justify-between">
-          <div className="h-10 w-1/2 mb-2 rounded-xl bg-gray-300 "></div>
-        </div>  
-        <div className="flex flex-col">
-        <div className="h-8 w-1/3 ml-10 mb-2 mb-2 rounded-xl bg-gray-300"></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300"></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-          <div className="h-8 w-1/3 ml-10  mt-10 mb-2 mb-2 rounded-xl bg-gray-300"></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-          <div className="h-4 mb-2 rounded-xl bg-gray-300 "></div>
-        </div>
-      </div>
-    );
-  }
-
-  // Display the actual values once they have been loaded
   return (
-    <div className="w-full  flex grow flex-col overflow-y-auto mb-2 font-inter text-textC text-3xl">
+    <div className="w-full flex grow flex-col overflow-y-auto mb-2 font-inter text-textC">
       {/* Display the title */}
-      <h1 className=" font-bold text-3xl italic">{topic}</h1>
+      <h1 className=" font-bold text-4xl">{topic}</h1>
       {/* Display the header, subheader, and page */}
-      <div className="flex flex-row justify-between">
-        <h2 className="text-xl pl-4 font-bold">{details[0].header}</h2>
-        <h3 className="text-normal pl-8 italic">{details[0].subheader}</h3>
-        <p className="text-sm pl-20 ">{details[0].page}</p>
-      </div>
-      </div>
+      {details.length > 0 && details[0].header && (
+        <div className="flex flex-row justify-between">
+          <h2 className="text-xl pl-4 font-bold">{details[0].header}</h2>
+          <h3 className="text-normal pl-8 italic">{details[0].subheader}</h3>
+          <p className="text-sm pl-20 ">{details[0].page}</p>
+        </div>
+      )}
+    </div>
   );
 };
 
